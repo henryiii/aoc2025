47a735987c1d479fde41f0f6435c8a44bcdab7ce
Henry Schreiner <henryfs@princeton.edu>
2025-12-03 00:54:26 -0500

    refactor: use Uint8Array and subarrays
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/day03.ts
+++ src/day03.ts
@@ -1,12 +1,12 @@
-function pick(arr: number[], start: number, k: number): number {
+function pick(arr: Uint8Array, k: number): number {
   if (k === 0) return 0;
 
   const end = arr.length - k;
 
   for (let d = 9; d >= 0; d--) {
-    for (let i = start; i <= end; i++) {
+    for (let i = 0; i <= end; i++) {
       if (arr[i] === d) {
-        return d * 10 ** (k - 1) + pick(arr, i + 1, k - 1);
+        return d * 10 ** (k - 1) + pick(arr.subarray(i + 1), k - 1);
       }
     }
   }
@@ -14,14 +14,16 @@ function pick(arr: number[], start: number, k: number): number {
   throw new Error("unreachable");
 }
 
-function parse(input: string): number[][] {
-  return input.split("\n").map((line) => [...line].map(Number));
+function parse(input: string): Uint8Array[] {
+  return input
+    .split("\n")
+    .map((line) => Uint8Array.from(line, (char) => parseInt(char, 10)));
 }
 
 export function solve_a(input: string): number {
-  return parse(input).reduce((a, b) => a + pick(b, 0, 2), 0);
+  return parse(input).reduce((a, b) => a + pick(b, 2), 0);
 }
 
 export function solve_b(input: string): number {
-  return parse(input).reduce((sum, arr) => sum + pick(arr, 0, 12), 0);
+  return parse(input).reduce((sum, arr) => sum + pick(arr, 12), 0);
 }

00d9548a5099c0677f325c6d99b87828403037a0
Henry Schreiner <henryfs@princeton.edu>
2025-12-05 01:33:32 -0500

    refactor: pull out input day 5
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/day05.ts
+++ src/day05.ts
@@ -29,14 +29,18 @@ function merge_ranges(ranges: Range[]): Range[] {
   return merged;
 }
 
-export function solve_a(input: string): number {
+function ranges_ingredients(input: string): [Range[], number[]] {
   const [ranges_str, ingredients_str] = input.split("\n\n");
   const ranges = ranges_str.split("\n").map((line) => {
     const [min, max] = line.split("-").map(Number);
     return new Range(min, max);
   });
   const ingredients = ingredients_str.split("\n").map(Number);
+  return [ranges, ingredients];
+}
 
+export function solve_a(input: string): number {
+  const [ranges, ingredients] = ranges_ingredients(input);
   return ingredients.reduce(
     (sum, ingredient) =>
       sum + (ranges.some((range) => range.contains(ingredient)) ? 1 : 0),
@@ -45,11 +49,7 @@ export function solve_a(input: string): number {
 }
 
 export function solve_b(input: string): number {
-  const [ranges_str] = input.split("\n\n");
-  const ranges = ranges_str.split("\n").map((line) => {
-    const [min, max] = line.split("-").map(Number);
-    return new Range(min, max);
-  });
+  const [ranges] = ranges_ingredients(input);
   const merged = merge_ranges(ranges);
   return merged.reduce((sum, range) => sum + range.length, 0);
 }

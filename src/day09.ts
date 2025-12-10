5ecd057ac86f74fb1b3a4308bcb27130ea2f3140
Henry Schreiner <henryfs@princeton.edu>
2025-12-09 22:53:39 -0500

    feat: day 9b
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/day09.ts
+++ src/day09.ts
@@ -16,9 +16,48 @@ export function solve_a(input: string): number {
   );
 }
 
+function inside_path(
+  corners: Coords[],
+  first: Coords,
+  second: Coords,
+): boolean {
+  const [x1, y1] = first;
+  const [x2, y2] = second;
+  const minX = Math.min(x1, x2);
+  const maxX = Math.max(x1, x2);
+  const minY = Math.min(y1, y2);
+  const maxY = Math.max(y1, y2);
+
+  for (let i = 0; i < corners.length; i++) {
+    const [xA, yA] = corners[i];
+    const [xB, yB] = corners[(i + 1) % corners.length];
+
+    if (
+      (xA === xB &&
+        xA > minX &&
+        xA < maxX &&
+        Math.max(Math.min(yA, yB), minY) < Math.min(Math.max(yA, yB), maxY)) ||
+      (yA === yB &&
+        yA > minY &&
+        yA < maxY &&
+        Math.max(Math.min(xA, xB), minX) < Math.min(Math.max(xA, xB), maxX))
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
 export function solve_b(input: string): number {
-  return input
-    .split("\n")
-    .map(Number)
-    .reduce((a, b) => a * b, 1);
+  const corners = input.split("\n").map(toCoords);
+  return corners.reduce(
+    (max, corner, i) =>
+      Math.max(
+        max,
+        ...corners
+          .slice(i + 1)
+          .map((c) => (inside_path(corners, corner, c) ? area(corner, c) : 0)),
+      ),
+    0,
+  );
 }

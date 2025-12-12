fda40de8e255ef247a068626c22b0654ad314f89
Henry Schreiner <henryfs@princeton.edu>
2025-12-12 11:02:01 -0500

    chore: simplify a bit
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

--- src/day11.ts
+++ src/day11.ts
@@ -33,29 +33,30 @@ export function solve_a(input: string): number {
 function countPathsDacFFT(
   graph: Map<string, string[]>,
   node: string,
-  cache: Map<string, number>,
+  cache = new Map<string, number>(),
   visited = new Set<string>(),
   daq = false,
   fft = false,
 ): number {
   const cacheKey = `${node}|${daq ? 1 : 0}|${fft ? 1 : 0}`;
   if (cache.has(cacheKey)) return cache.get(cacheKey) || 0;
-  if (node === "dac") daq = true;
-  if (node === "fft") fft = true;
+
   visited.add(node);
-  let result: number;
+
+  daq ||= node === "dac";
+  fft ||= node === "fft";
+
+  let result = 0;
   if (node === "out") {
     result = daq && fft ? 1 : 0;
-    visited.delete(node);
-    cache.set(cacheKey, result);
-    return result;
-  }
-  result = 0;
-  for (const neighbor of graph.get(node) || []) {
-    if (!visited.has(neighbor)) {
-      result += countPathsDacFFT(graph, neighbor, cache, visited, daq, fft);
+  } else {
+    for (const neighbor of graph.get(node) || []) {
+      if (!visited.has(neighbor)) {
+        result += countPathsDacFFT(graph, neighbor, cache, visited, daq, fft);
+      }
     }
   }
+
   visited.delete(node);
   cache.set(cacheKey, result);
   return result;
@@ -63,6 +64,5 @@ function countPathsDacFFT(
 
 export function solve_b(input: string): number {
   const graph = parseGraph(input);
-  const cache = new Map<string, number>();
-  return countPathsDacFFT(graph, "svr", cache);
+  return countPathsDacFFT(graph, "svr");
 }
